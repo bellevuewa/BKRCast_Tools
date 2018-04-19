8c0df86913380bf004262c631c740030ae7fca83
Hu <hdong@bellevuewa.gov>
2018-04-19 15:24:04 -0700

    Allow universally increase number of jobs by a percentage. Create a parcel file summary tool.

--- Adjust_Employment_by_tract_contral_total.py
+++ Adjust_Employment_by_tract_contral_total.py
@@ -16,6 +16,8 @@ input_Parcel_File_Name = r"BKRCast_Parcel_1995data.csv"
 input_hh_person_file_name = r"hh_and_persons.h5"
 Output_Parcel_File_Name = "parcels_urbansim_1995.txt"
 
+universal_factor_for_uncovered_ESD_data = 1.075   # can b
+
 def h5_to_df(h5_file, group_name):
     """
     Converts the arrays in a H5 store to a Pandas DataFrame. 
@@ -37,7 +39,7 @@ JOB_CATEGORY = ['EMPEDU_P', 'EMPFOO_P', 'EMPGOV_P', 'EMPIND_P', 'EMPMED_P', 'EMP
 parcels['EMPTOT_P'] = 0
 
 for job_cat in JOB_CATEGORY:
-    parcels[job_cat] = parcels[job_cat] * ratio
+    parcels[job_cat] = parcels[job_cat] * ratio * universal_factor_for_uncovered_ESD_data
     parcels['EMPTOT_P'] = parcels['EMPTOT_P'] + parcels[job_cat]
 
 hdf_file = h5py.File(os.path.join(input_Parcel_Folder, input_hh_person_file_name), "r")
@@ -49,6 +51,7 @@ parcels['HH_P'] = parcels['hhno']
 parcels = parcels.reset_index()
 parcels.drop(columns_drop, inplace = True, axis = 1)
 parcels.drop('hhno', inplace = True, axis = 1)
+parcels.sort_values('PARCELID', inplace = True)
 
 parcels.to_csv(os.path.join(input_Parcel_Folder, Output_Parcel_File_Name), index = False, sep = ' ')
 print 'done'
---
+++ parcel_file_summary.py
@@ -0,0 +1,32 @@
+import os, sys
+sys.path.append(os.getcwd())
+import pandas as pd
+import numpy as np
+
+Original_Parcel_Folder = r"Z:\Modeling Group\BKRCast\Job Conversion Test\parcel_level\test9"
+Original_ESD_Parcel_File_Name = r"parcels_urbansim.txt"
+TAZ_Subarea_File_Name = r"TAZ_subarea.csv"
+
+Output_Field = ['EMPEDU_P', 'EMPFOO_P', 'EMPGOV_P', 'EMPIND_P', 'EMPMED_P', 'EMPOFC_P', 'EMPOTH_P', 'EMPRET_P', 'EMPSVC_P', 'EMPTOT_P', 'HH_P']
+
+print "Loading input files ..."
+parcels = pd.DataFrame.from_csv(os.path.join(Original_Parcel_Folder, Original_ESD_Parcel_File_Name), sep = " ", index_col = "PARCELID")
+taz_subarea = pd.DataFrame.from_csv(os.path.join(Original_Parcel_Folder, TAZ_Subarea_File_Name), sep = ",", index_col = "TAZNUM")
+
+parcels = parcels.join(taz_subarea, on = 'TAZ_P')
+
+summary_by_jurisdiction = parcels.groupby('Jurisdiction')[Output_Field].sum()
+print "Exporting \"summary_by_jurisdiction.csv\""
+summary_by_jurisdiction.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_jurisdiction.csv"))
+summary_by_taz = parcels.groupby('TAZ_P')[Output_Field].sum()
+print "Exporting \"summary_by_TAZ.csv\""
+summary_by_taz.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_TAZ.csv")) 
+print "Exporting \"summary_by_subarea.csv\""
+summary_by_subarea = parcels[parcels['Subarea'] > 0].groupby('Subarea')[Output_Field].sum()
+taz_subarea = parcels[['Subarea', 'SubareaName']].drop_duplicates()
+taz_subarea.set_index('Subarea', inplace = True)
+summary_by_subarea = summary_by_subarea.join(taz_subarea['SubareaName'])
+summary_by_subarea.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_subarea.csv"))
+
+
+
