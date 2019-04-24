6712492410ec125fafa77689caf95482976e2d81
Hu Dong <HDong@bellevuewa.gov>
2019-04-24 14:39:58 -0700

    add comments to extract_subset_of_parcel_file.py

--- extract_subset_of_parcel_file.py
+++ extract_subset_of_parcel_file.py
@@ -1,26 +1,26 @@
-# Extract a subset of parcel fille and export it to an external file.
+# Extract a subset of parcel file, defined by a list of TAZ numbers or parcel IDs, and export it to an external file.
 
 import os, sys
 sys.path.append(os.getcwd())
 import pandas as pd
 import numpy as np
 
+
+### Configuration
 SELECT_BY_TAZ = True
 SELECT_BY_PARCEL = False
-
 Original_Parcel_Folder = r"Z:\Modeling Group\BKRCast\Job Conversion Test\parcel_level\test11"
 Original_ESD_Parcel_File_Name = r"parcels_urbansim.txt"
 TAZ_Subarea_File_Name = r"TAZ_subarea.csv"
-Subset_definition_file = r"Bel_CBD_TAZ.csv"
+Subset_definition_file = r"Bel_CBD_TAZ.csv"   # TAZ list or ParcelID list
 Outputfile = '2014_BelCBD_Parcels.txt'
+###
 
 print "Loading input files ..."
 parcels_df = pd.DataFrame.from_csv(os.path.join(Original_Parcel_Folder, Original_ESD_Parcel_File_Name), sep = " ", index_col = "PARCELID")
 taz_subarea_df = pd.DataFrame.from_csv(os.path.join(Original_Parcel_Folder, TAZ_Subarea_File_Name), sep = ",", index_col = "TAZNUM")
 subset_def_df = pd.DataFrame.from_csv(os.path.join(Original_Parcel_Folder, Subset_definition_file), sep = ',')
 parcels_df = parcels_df.join(taz_subarea_df, on = 'TAZ_P')
-# parcels_selected_df = parcels_df[parcels_df['Jurisdiction'] == 'BELLEVUE']
-# parcels_selected_df = parcels_df[parcels_df['TAZ_P'] == 879]
 
 if SELECT_BY_TAZ: 
     parcels_selected_df = parcels_df.join(subset_def_df, on = 'TAZ_P', how = 'right')
