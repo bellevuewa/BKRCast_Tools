c789ace7da0f11ffc827290245ce17fc7c513cd6
Hu Dong <HDong@bellevuewa.gov>
2019-05-30 16:25:04 -0700

    clean up a little bit on parcel file summary

--- parcel_file_summary.py
+++ parcel_file_summary.py
@@ -3,9 +3,13 @@ sys.path.append(os.getcwd())
 import pandas as pd
 import numpy as np
 
-Original_Parcel_Folder = r"Z:\Modeling Group\BKRCast\2018LU\Test1"
-Original_ESD_Parcel_File_Name = r"parcels_urbansim_Updated.txt"
+# Summarize parcel urbansim file to TAZ, subarea and city level.
+
+### Configuration
+Original_Parcel_Folder = r"Z:\Modeling Group\BKRCast\2035Parcel_Sqft_based"
+Original_ESD_Parcel_File_Name = r"BKRCastFuture_adjusted_parcels_urbansim_test.txt"
 TAZ_Subarea_File_Name = r"Z:\Modeling Group\BKRCast\2018LU\TAZ_subarea.csv"
+###
 
 Output_Field = ['EMPEDU_P', 'EMPFOO_P', 'EMPGOV_P', 'EMPIND_P', 'EMPMED_P', 'EMPOFC_P', 'EMPOTH_P', 'EMPRET_P', 'EMPSVC_P', 'EMPTOT_P', 'HH_P']
 
@@ -17,16 +21,16 @@ parcels = parcels.join(taz_subarea, on = 'TAZ_P')
 
 summary_by_jurisdiction = parcels.groupby('Jurisdiction')[Output_Field].sum()
 print "Exporting \"summary_by_jurisdiction.csv\""
-summary_by_jurisdiction.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_jurisdiction.csv"))
+summary_by_jurisdiction.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_jurisdiction_test.csv"))
 summary_by_taz = parcels.groupby('TAZ_P')[Output_Field].sum()
 print "Exporting \"summary_by_TAZ.csv\""
-summary_by_taz.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_TAZ.csv")) 
+summary_by_taz.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_TAZ_test.csv")) 
 print "Exporting \"summary_by_subarea.csv\""
 summary_by_subarea = parcels[parcels['Subarea'] > 0].groupby('Subarea')[Output_Field].sum()
 taz_subarea = parcels[['Subarea', 'SubareaName']].drop_duplicates()
 taz_subarea.set_index('Subarea', inplace = True)
 summary_by_subarea = summary_by_subarea.join(taz_subarea['SubareaName'])
-summary_by_subarea.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_subarea.csv"))
+summary_by_subarea.to_csv(os.path.join(Original_Parcel_Folder, "summary_by_subarea_test.csv"))
 
 
 
