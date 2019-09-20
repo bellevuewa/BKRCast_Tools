e7ca2da78dc877ca14491d85c439bfe46a292543
Hu Dong <HDong@bellevuewa.gov>
2019-09-20 12:54:29 -0700

    Fixed a bug that could drop some households from local estimate.

--- TemoraryCode.py
+++ TemoraryCode.py
@@ -3,13 +3,16 @@ sys.path.append(os.getcwd())
 import pandas as pd
 import numpy as np
 
-filename = r'Z:\Modeling Group\BKRCast\2014ESD\parcels_urbansim.txt'
+filename = r'D:\BKR0V1-1-newcap-newpop-coeff5-nettreaks2\outputs\_person.tsv'
 print "Loading input files ..."
-parcels = pd.DataFrame.from_csv(filename, sep = " ", index_col = "PARCELID")
+persons = pd.DataFrame.from_csv(filename, sep = '\t')
 
-#sum_by_county = parcels.groupby('JURISDICTION').sum()
-sum = parcels.sum()
-sum.to_csv(r'Z:\Modeling Group\BKRCast\2014ESD\LUsummary.txt')
+parcels = pd.DataFrame.from_csv(r"Z:\Modeling Group\BKRCast\2014_ParkingCost\Half_parking_Cost_Bellevue_newpop\parcels_urbansim.txt", sep = ' ')
+jobs_by_taz = parcels.groupby('TAZ_P').sum()
+
+persons_by_taz = persons.groupby('pwtaz').sum()
+
+
 print 'Done'
 
 #sum_by_county.to_csv(r'Z:\Modeling Group\BKRCast\2018LU\2018sqft_by_jurisdiction.csv')
