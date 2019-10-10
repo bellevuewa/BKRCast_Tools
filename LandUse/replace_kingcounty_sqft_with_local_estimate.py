76450389304a0f72db1ba18fe3cbda174270f45c
Hu Dong <HDong@bellevuewa.gov>
2019-10-10 14:58:17 -0700

    add jurisdiction info to replace_kingcounty_sqft_with_local_estimate.py

--- LandUse/replace_kingcounty_sqft_with_local_estimate.py
+++ LandUse/replace_kingcounty_sqft_with_local_estimate.py
@@ -18,18 +18,23 @@ import ntpath
 ### configuration
 Original_Parcel_File_Name = r"Z:\Modeling Group\BKRCast\2018LU\2018_kingcounty_LU_by_parcel.csv"
 Output_Parcel_Folder = r"Z:\Modeling Group\BKRCast\2018LU"
+parcel_lookup_File_Name = r'I:\Modeling and Analysis Group\07_ModelDevelopment&Upgrade\NextgenerationModel\BasicData\parcel_TAZ_2014_lookup.csv'
+
 Subset_Parcels_File_Name = r"Z:\Modeling Group\BKRCast\2018LU\2018_Bellevue_LU_by_parcel.csv"
 Output_Parcel_File_Name = "updated_2018_kingcounty_LU_by_parcel.csv"
 ###
 
 CONVERSION_LEVEL = ['verylow', 'low', 'med', 'high', 'veryhigh']
-EMPLOYMENT_TYPE = ['EDU', 'FOO', 'GOV', 'IND', 'MED', 'OFC', 'OTH', 'RET', 'SVC']
+EMPLOYMENT_TYPE = ['EDU', 'FOO', 'GOV', 'IND', 'MED', 'OFC', 'OTH', 'RET', 'SVC', 'NoEmp']
 
 if not os.path.exists(Output_Parcel_Folder):
     os.makedirs(Output_Parcel_Folder)
 
 lu_type_list = []
 for lu_type in EMPLOYMENT_TYPE:
+    if lu_type == 'NoEmp':
+        lu_type_list.append(lu_type.upper())
+        continue
     for level in CONVERSION_LEVEL:
         new_lu_type = lu_type + "_" + level
         new_lu_type = new_lu_type.upper()
@@ -37,26 +42,33 @@ for lu_type in EMPLOYMENT_TYPE:
 
 print "Loading input files ..."
 parcels = pd.read_csv(Original_Parcel_File_Name, sep = ',')
+parcels = parcels.loc[~parcels['PSRC_ID'].isnull()]
 parcels.columns = [i.upper() for i in parcels.columns]
 parcels = parcels.groupby('PSRC_ID')[lu_type_list].sum()
 print 'original parcel total sqft: ', parcels[lu_type_list].sum().sum()
 
+parcel_lookup_df = pd.read_csv(parcel_lookup_File_Name, sep = ',', low_memory = False)
+parcel_lookup_df.columns = [i.upper() for i in parcel_lookup_df.columns]
+
 subset_parcels = pd.read_csv(Subset_Parcels_File_Name, sep = ',')
+subset_parcels = subset_parcels.loc[~subset_parcels['PSRC_ID'].isnull()]
 subset_parcels.columns = [i.upper() for i in subset_parcels.columns]
 subset_parcels = subset_parcels.groupby('PSRC_ID')[lu_type_list].sum()
 updated_parcels = parcels.loc[~parcels.index.isin(subset_parcels.index)]
 updated_parcels = updated_parcels.append(subset_parcels) 
+updated_parcels = updated_parcels.fillna(0)
+final_updated_parcels = updated_parcels.reset_index().merge(parcel_lookup_df[['PSRC_ID', 'JURISDICTION']], how = 'left', left_on = 'PSRC_ID', right_on = 'PSRC_ID')
 
 subset_new_tot_sqft = subset_parcels[lu_type_list].sum().sum()
 print 'Subset new sqft {0:.0f}'.format(subset_new_tot_sqft)
 subset_old_tot_sqft = parcels.loc[parcels.index.isin(subset_parcels.index), lu_type_list].sum().sum()
 print 'Subset old sqft {0:.0f}'.format(subset_old_tot_sqft)
 
-updated_tot_sqft = updated_parcels[lu_type_list].sum().sum()
+updated_tot_sqft = final_updated_parcels[lu_type_list].sum().sum()
 print 'Updated total sqft {0:.0f}'.format(updated_tot_sqft)
 
 print "Exporting updated urbansim parcel file ..."
-updated_parcels.to_csv(os.path.join(Output_Parcel_Folder, Output_Parcel_File_Name), sep = ',')
+final_updated_parcels.to_csv(os.path.join(Output_Parcel_Folder, Output_Parcel_File_Name), sep = ',')
 
 
 
