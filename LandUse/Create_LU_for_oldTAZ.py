d2d0ca82414bf91ea30f01d345749921c87aab61
Hu Dong <HDong@bellevuewa.gov>
2019-10-29 17:01:33 -0700

    changed aggregation convention.

--- LandUse/Create_LU_for_oldTAZ.py
+++ LandUse/Create_LU_for_oldTAZ.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import os
 
-parcel_file = r'Z:\Modeling Group\BKRCast\2018LU\parcels_urbansim.txt'
+'''
+   this program is used to aggregate number of jobs and households from parcel file to the old BKR TAZ system. 
+'''
+
+### configuration
+parcel_file = r'Z:\Modeling Group\BKRCast\2020concurrencyPretest\parcels_urbansim.txt'
 lookup_file = r'I:\Modeling and Analysis Group\07_ModelDevelopment&Upgrade\NextgenerationModel\BasicData\BKRCast_Parcels_BKRTAZ.csv'
-output_file = r'Z:\Modeling Group\BKRCast\2018LU\2018_LU_by_oldTAZ.csv'
+output_file = r'Z:\Modeling Group\BKRCast\2020concurrencyPretest\2020_concurrency_Pretest_LU_by_oldTAZ.csv'
+### end of configuration
+
 
 parcel_df = pd.read_csv(parcel_file, sep = ' ', low_memory = False)
 lookup_df = pd.read_csv(lookup_file, sep = ',', low_memory = False)
@@ -14,9 +21,10 @@ parcel_df = parcel_df.merge(lookup_df, how = 'left', left_on = 'PARCELID', right
 
 lu_by_oldtaz_df = parcel_df.groupby('TAZNUM')[JOB_CATEGORY].sum()
 
-lu_by_oldtaz_df['Office'] = lu_by_oldtaz_df['EMPGOV_P'] + lu_by_oldtaz_df['EMPOFC_P'] + lu_by_oldtaz_df['EMPSVC_P'] + lu_by_oldtaz_df['EMPMED_P']
+# convention of aggregation
+lu_by_oldtaz_df['FIRES'] = lu_by_oldtaz_df['EMPOFC_P'] + lu_by_oldtaz_df['EMPSVC_P'] 
+lu_by_oldtaz_df['Inst'] =  lu_by_oldtaz_df['EMPGOV_P'] + lu_by_oldtaz_df['EMPEDU_P'] + lu_by_oldtaz_df['EMPMED_P']
 lu_by_oldtaz_df['Retail'] = lu_by_oldtaz_df['EMPRET_P'] + lu_by_oldtaz_df['EMPFOO_P'] + lu_by_oldtaz_df['EMPOTH_P']
-lu_by_oldtaz_df['Edu'] = lu_by_oldtaz_df['EMPEDU_P']
 lu_by_oldtaz_df['Ind'] = lu_by_oldtaz_df['EMPIND_P']
 lu_by_oldtaz_df['HHs'] = lu_by_oldtaz_df['HH_P']
 
