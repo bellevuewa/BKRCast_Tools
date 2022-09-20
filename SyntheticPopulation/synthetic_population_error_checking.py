cb43f85f04442254f5da7bf3e187a5aa8d4506d9
Hu Dong <HDong@bellevuewa.gov>
2022-09-20 12:59:18 -0700

    check duplicated hhno

--- SyntheticPopulation/synthetic_population_error_checking.py
+++ SyntheticPopulation/synthetic_population_error_checking.py
@@ -8,8 +8,8 @@ import utility
 
 ############# confiuration ###############
 ## input files
-hh_person_folder = r'I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\TFP\2033_horizon_year'
-hh_person_file = '2033TFP_hh_and_persons.h5'
+hh_person_folder = r'I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\2019-test\new_allocation_approach'
+hh_person_file = '2019_hh_and_persons.h5'
 
 ## output files
 error_hhs_file = 'hhs.f'
@@ -28,5 +28,12 @@ error_persons_df = person_df[person_df.isnull().any(axis = 1)]
 
 error_hhs_df.to_csv(os.path.join(hh_person_folder, error_hhs_file), sep = ',')
 error_persons_df.to_csv(os.path.join(hh_person_folder, error_persons_file), sep = ',')
+duplicated_hhs_df = hh_df[hh_df.duplicated('hhno', keep = False)]
+if duplicated_hhs_df.shape[0] == 0:
+    print('no duplicated household id (hhno) is found.')
+else:
+    print('found duplicated household ids (hhno). check out duplicated_hhs.csv for details.')
+    duplicated_hhs_df.to_csv(os.path.join(hh_person_folder, 'duplicated_hhs.csv'), index = False)
+
 
 print('Done')
