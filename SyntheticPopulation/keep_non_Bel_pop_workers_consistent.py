6172a8767d02d213aefc9fa75734d3d9ff7b71e8
Hu Dong <HDong@bellevuewa.gov>
2023-03-04 17:27:32 -0800

    two tools for the complan synthetic population

--- SyntheticPopulation/keep_non_Bel_pop_workers_consistent.py
+++ SyntheticPopulation/keep_non_Bel_pop_workers_consistent.py
@@ -1,1 +1,65 @@
+import pandas as pd
+import h5py
+import os
+import utility
+
+
+working_folder = r'I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\Complan\complan2044\WFH\Alt3-30%WFH'
+bel_syn_pop_file = r"final_combined_complan_alt3_hh_and_persons_forWFH_30%.h5"
+non_bel_syn_pop_file = r"I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\Complan\complan2044\WFH\NA-30%WFH\final_combined_COB_renumbered_2044complan_na_hh_and_persons_forWFH_30%.h5"
+parcel_filename = r'I:\Modeling and Analysis Group\07_ModelDevelopment&Upgrade\NextgenerationModel\BasicData\parcel_TAZ_2014_lookup.csv'
+non_bel_converted_worker_file = r"I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\Complan\complan2044\WFH\NA-30%WFH\converted_non_workers.csv"
+bel_converted_worker_file = 'converted_non_workers.csv'
+# output
+final_output_pop_file = 'final_combined_complan_alt3_hh_and_persons_forWFH_30%_outside_bel_consistent_with_NA.h5'
+converted_workers_file = 'final_converted_non_workers.csv'
+
+subarea = ['BELLEVUE']
+
+print('Loading hh and person file...')
+bel_hdf_file = h5py.File(os.path.join(working_folder, bel_syn_pop_file), "r")
+bel_person_df = utility.h5_to_df(bel_hdf_file, 'Person')
+bel_hh_df = utility.h5_to_df(bel_hdf_file, 'Household')
+
+non_bel_hdf_file = h5py.File(non_bel_syn_pop_file, 'r')
+non_bel_person_df = utility.h5_to_df(non_bel_hdf_file, 'Person')
+non_bel_hh_df = utility.h5_to_df(non_bel_hdf_file, 'Household')
+
+parcel_df = pd.read_csv(parcel_filename, low_memory=False) 
+
+## remove hhs and persons in Bellevue
+non_bel_hh_df = non_bel_hh_df.merge(parcel_df[['PSRC_ID', 'Jurisdiction']], how = 'left', left_on = 'hhparcel', right_on = 'PSRC_ID')
+non_bel_hh_df = non_bel_hh_df.loc[~non_bel_hh_df['Jurisdiction'].isin(subarea)].copy()
+non_bel_person_df = non_bel_person_df.loc[non_bel_person_df['hhno'].isin(non_bel_hh_df['hhno'])].copy()
+
+bel_hh_df = bel_hh_df.merge(parcel_df[['PSRC_ID', 'Jurisdiction']], how = 'left', left_on = 'hhparcel', right_on = 'PSRC_ID')
+bel_hh_df = bel_hh_df.loc[bel_hh_df['Jurisdiction'].isin(subarea)].copy()
+bel_person_df = bel_person_df.loc[bel_person_df['hhno'].isin(bel_hh_df['hhno'])].copy()
+
+
+final_hhs_df = non_bel_hh_df.append(bel_hh_df)
+final_hhs_df = final_hhs_df.drop(['PSRC_ID', 'Jurisdiction'], axis = 1)
+final_persons_df = non_bel_person_df.append(bel_person_df)
+
+output_h5_file = h5py.File(os.path.join(working_folder, final_output_pop_file), 'w')
+utility.df_to_h5(final_hhs_df, output_h5_file, 'Household')
+utility.df_to_h5(final_persons_df, output_h5_file, 'Person')
+output_h5_file.close()
+
+
+bel_non_workers_df = pd.read_csv(os.path.join(working_folder,  bel_converted_worker_file))
+non_bel_non_workers_df = pd.read_csv(non_bel_converted_worker_file)
+
+bel_non_workers_df  = bel_non_workers_df.merge(parcel_df[['PSRC_ID', 'Jurisdiction']], how = 'left', left_on = 'hhparcel', right_on = 'PSRC_ID')
+bel_non_workers_df  = bel_non_workers_df.loc[bel_non_workers_df['Jurisdiction'].isin(subarea)].copy()
+
+non_bel_non_workers_df  = non_bel_non_workers_df.merge(parcel_df[['PSRC_ID', 'Jurisdiction']], how = 'left', left_on = 'hhparcel', right_on = 'PSRC_ID')
+non_bel_non_workers_df  = non_bel_non_workers_df.loc[~non_bel_non_workers_df['Jurisdiction'].isin(subarea)].copy()
+
+final_non_workers_df = bel_non_workers_df.append(non_bel_non_workers_df)
+final_non_workers_df = final_non_workers_df.drop(['PSRC_ID', 'Jurisdiction'], axis = 1)
+final_non_workers_df.to_csv(os.path.join(working_folder, converted_workers_file), index = False)
+utility.backupScripts(__file__, os.path.join(working_folder, os.path.basename(__file__)))
+
+print('H5 exported.')
 
--- SyntheticPopulation/merge_two_synthetic_populations_for_complan.py
+++ SyntheticPopulation/merge_two_synthetic_populations_for_complan.py
@@ -9,17 +9,21 @@ import utility
 
 
 ### configuration
-working_folder = r'I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\2021Concurrency'
-base_syn_pop_file = r'I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\2020-BKR\2020_hh_and_persons.h5'
-growth_syn_pop_file = '2021Concurrecy_growth_hh_and_persons.h5'
-
+working_folder = r'I:\Modeling and Analysis Group\01_BKRCast\BKRPopSim\PopulationSim_BaseData\Complan\complan2044\Alt3'
+base_syn_pop_file = r'final_combined_COB_renumbered_2044complan_na_hh_and_persons.h5'
+growth_syn_pop_file = 'complan_alt3_hh_and_persons.h5'
+parcel_filename = r'I:\Modeling and Analysis Group\07_ModelDevelopment&Upgrade\NextgenerationModel\BasicData\parcel_TAZ_2014_lookup.csv'
 # output
-final_output_pop_file = '2021_concurrency_hh_and_persons.h5'
+final_output_pop_file = 'final_combined_complan_alt3_hh_and_persons.h5'
 
 ### end of configuration
 
-print 'Loading hh and person file...'
-base_hdf_file = h5py.File(base_syn_pop_file, "r")
+subarea = ['BELLEVUE']
+#subarea = ['BELLEVUE', 'KIRKLAND', 'REDMOND', 'BellevueFringe', 'KirklandFringe', 'RedmondFringe']
+
+
+print('Loading hh and person file...')
+base_hdf_file = h5py.File(os.path.join(working_folder, base_syn_pop_file), "r")
 base_person_df = utility.h5_to_df(base_hdf_file, 'Person')
 base_hh_df = utility.h5_to_df(base_hdf_file, 'Household')
 
@@ -27,26 +31,38 @@ growth_hdf_file = h5py.File(os.path.join(working_folder, growth_syn_pop_file), '
 growth_person_df = utility.h5_to_df(growth_hdf_file, 'Person')
 growth_hh_df = utility.h5_to_df(growth_hdf_file, 'Household')
 
+parcel_df = pd.read_csv(parcel_filename, low_memory=False) 
+
+
+## remove hhs and persons in Bellevue
+base_hh_df = base_hh_df.merge(parcel_df[['PSRC_ID', 'Jurisdiction']], how = 'left', left_on = 'hhparcel', right_on = 'PSRC_ID')
+base_hh_df_no_Bel = base_hh_df.loc[~base_hh_df['Jurisdiction'].isin(subarea)].copy()
+base_person_df_no_Bel = base_person_df.loc[base_person_df['hhno'].isin(base_hh_df_no_Bel['hhno'])].copy()
+
+growth_hh_df = growth_hh_df.merge(parcel_df[['PSRC_ID', 'Jurisdiction']], how = 'left', left_on = 'hhparcel', right_on = 'PSRC_ID')
+growth_hh_df_Bel = growth_hh_df.loc[growth_hh_df['Jurisdiction'].isin(subarea)].copy()
+growth_person_df_Bel = growth_person_df.loc[growth_person_df['hhno'].isin(growth_hh_df_Bel['hhno'])].copy()
+
 #find the max hhid from base_hh_df
-max_base_hhid = max(base_hh_df['hhno'])
+max_base_hhid = max(base_hh_df_no_Bel['hhno'])
 next_hhno = max_base_hhid
-for hhno in growth_hh_df['hhno']:
+for hhno in growth_hh_df_Bel['hhno']:
     next_hhno = next_hhno + 1
-    growth_hh_df.loc[growth_hh_df['hhno'] == hhno, 'hhno'] = next_hhno
-    growth_person_df.loc[growth_person_df['hhno'] == hhno, 'hhno'] = next_hhno
+    growth_hh_df_Bel.loc[growth_hh_df_Bel['hhno'] == hhno, 'hhno'] = next_hhno
+    growth_person_df_Bel.loc[growth_person_df_Bel['hhno'] == hhno, 'hhno'] = next_hhno
 
-    #print 'household ', hhno, ' in the growth dataset is now changed to ', next_hhno
-
-final_hhs_df = base_hh_df.append(growth_hh_df)
-final_persons_df = base_person_df.append(growth_person_df)
+final_hhs_df = base_hh_df_no_Bel.append(growth_hh_df_Bel)
+final_hhs_df = final_hhs_df.drop(['PSRC_ID', 'Jurisdiction'], axis = 1)
+final_persons_df = base_person_df_no_Bel.append(growth_person_df_Bel)
 
 output_h5_file = h5py.File(os.path.join(working_folder, final_output_pop_file), 'w')
 utility.df_to_h5(final_hhs_df, output_h5_file, 'Household')
 utility.df_to_h5(final_persons_df, output_h5_file, 'Person')
 output_h5_file.close()
-print 'H5 exported.'
 
-print 'Done.'
+utility.backupScripts(__file__, os.path.join(working_folder, os.path.basename(__file__)))
+
+print('H5 exported.')
 
 
 
